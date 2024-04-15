49b97f1e951145644476f76dc68424e835c4f945
abdullohnajmiddinov <abdullohnajmiddinov45@gmail.com>
2024-04-15 16:19:43 +0300

    done

---
+++ main.js
@@ -0,0 +1,60 @@
+// 1-zadacha
+// let son1 = 5;
+// let son2 = 10;  
+// console.log(son1+son2);
+
+
+
+// 2-zadacha
+// let a = '12';
+// a = Number(a);
+// console.log(a);
+
+
+
+// 3-zadacha
+// let son = Math.floor(Math.random() * 100) + 1; // 1 dan 100 gacha bo'lgan butun sonlar
+// console.log("Random son:", son);
+// if (son % 2 === 0) {
+//     console.log("Bu son juft.");
+// } else {
+//     console.log("Bu son toq.");
+// }
+
+
+
+// 4-zadacha
+// let soz = prompt("Iltimos, biror so'z kiriting:");
+
+// if (soz.toLowerCase() === "mars") {
+//     console.log(soz);
+// } else {
+//     console.log("Mars It school");
+// }
+
+
+
+// 5-zadacha
+// let arr =['Abdulloh','nok','Navfal','anor','Mars','MArs-bu kelajak']
+// let arr1=[]
+// for (let i = 0; i < arr.length; i++) {
+//   arr1.push(arr.filter((item)=>item.length>=5))
+// }
+// console.log(arr1);
+
+
+
+// 6-zadacha
+// let juftSonlar = [];
+// let toqSonlar = [];
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         juftSonlar.push(i);
+//     } else {
+//         toqSonlar.push(i);
+//     }
+// }
+
+// console.log("Juft sonlar:", juftSonlar);
+// console.log("Toq sonlar:", toqSonlar);
